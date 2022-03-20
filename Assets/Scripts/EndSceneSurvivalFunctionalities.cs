f90dfa3f5b36ce460acda34fc4741da6ea07ff92
SamGomes <samuel.gomes@tecnico.ulisboa.pt>
2022-03-20 02:15:19 +0000

    Added some stuff to final questionnaire.

--- Assets/Scripts/EndSceneSurvivalFunctionalities.cs
+++ Assets/Scripts/EndSceneSurvivalFunctionalities.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using TMPro;
@@ -6,24 +7,31 @@ using UnityEngine.UI;
 
 public class EndSceneSurvivalFunctionalities: MonoBehaviour
 {
+    public GameObject thankYouObj;
     public GameObject engQuestionnaire;
     public Button submitButton;
-    private Slider[] engQuestions;
+    private Slider[] engQuestionsSliders;
+    private GameObject[] engQuestions;
+    
     public void Start()
     {
         
-        engQuestions = FindObjectsOfType<Slider>();
+        engQuestionsSliders = engQuestionnaire.GetComponentsInChildren<Slider>();
+        engQuestions = GameObject.FindGameObjectsWithTag("Question");
+
         submitButton.onClick.AddListener(() => {
             
             float engValue = 0.0f;
-            for (int i = 0; i < engQuestions.Length; i++)
+            Dictionary<string,float> engQuestionnaireValues = new Dictionary<string,float>();
+            for (int i = 0; i < engQuestionsSliders.Length; i++)
             {
-                var currSlider = engQuestions[i];
+                var currSlider = engQuestionsSliders[i];
                 engValue += (currSlider.value/ 6.0f) / engQuestions.Length;
+                engQuestionnaireValues.Add(engQuestions[i].GetComponent<TextMeshProUGUI>().text, currSlider.value);
             }
         
             string path = "Assets/StreamingAssets/Results/GroupResults/"+GameGlobals.PlayerId+".txt";
-            string json = "{ \"engAnswers\"" + JsonConvert.SerializeObject(engQuestions)+
+            string json = "{ \"engAnswers\":" + JsonConvert.SerializeObject(engQuestionnaireValues)+
                           "\"abilityInc\": "+ GameGlobals.Score/ 10000.0f+
                           ",\"engagementInc\": "+engValue+"," +
                           "\"gradeInc\": "+0.5+"}";
@@ -32,6 +40,8 @@ public class EndSceneSurvivalFunctionalities: MonoBehaviour
             
             File.WriteAllText(path,json);
             submitButton.GetComponent<Image>().color = Color.green;
+            engQuestionnaire.gameObject.SetActive(false);
+            thankYouObj.SetActive(true);
             
         });
     }
